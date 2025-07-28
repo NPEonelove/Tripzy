8cb441fa187cc4a6c3e242c965fb7994cb16eb51
NPEonelove <124193334+NPEonelove@users.noreply.github.com>
2025-07-29 01:11:57 +0400

    fix(profile-service): fix CORS in profile-service

--- profile-service/src/main/java/com/meowlove/profileservice/controller/ProfileController.java
+++ profile-service/src/main/java/com/meowlove/profileservice/controller/ProfileController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/profiles")
