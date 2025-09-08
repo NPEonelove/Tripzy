73755d8e93a33ba62e376a4ad88b4cb8bffd38ce
NPEonelove <124193334+NPEonelove@users.noreply.github.com>
2025-09-08 10:58:11 +0400

    Update CustomGlobalFilter.java

--- api-gateway/src/main/java/com/meowlove/apigateway/filter/CustomGlobalFilter.java
+++ api-gateway/src/main/java/com/meowlove/apigateway/filter/CustomGlobalFilter.java
@@ -13,7 +13,6 @@ import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
