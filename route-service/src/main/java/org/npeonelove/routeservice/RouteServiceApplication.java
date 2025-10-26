bed897f66489b3b503aa446951feb9a5708e0eb6
NPEonelove <124193334+NPEonelove@users.noreply.github.com>
2025-10-26 16:46:29 +0400

    feat(route-service): update pom.xml, temporary disable this microservice

--- route-service/src/main/java/org/npeonelove/routeservice/RouteServiceApplication.java
+++ route-service/src/main/java/org/npeonelove/routeservice/RouteServiceApplication.java
@@ -3,10 +3,8 @@ package org.npeonelove.routeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
 @EnableDiscoveryClient
 public class RouteServiceApplication {
 
